9a3bae7defe0f001eca1710f7feae2c5904d2502
Muhammad Lutfi Rahmawan <>
2019-05-25 20:03:09 +0700

    Class Reference

---
+++ src/pboparkir/Referance.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboparkir;
+
+/**
+ *
+ * @author laptop
+ */
+public class Referance {
+    String nopol, kode;
+
+    public String getNopol() {
+        return nopol;
+    }
+
+    public void setNopol(String nopol) {
+        this.nopol = nopol;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+}
